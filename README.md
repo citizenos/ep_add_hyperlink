9e31fd4a9c6a9bdbc7de39d45b4b04449fad54a5
Ilmar Türk <ilmar.tyrk@gmail.com>
2025-03-09 01:00:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Etherpad Add Hyperlink Plugin
+![Publish Status](https://github.com/ether/ep_add_hyperlink/workflows/Node.js%20Package/badge.svg) ![Publish Status](https://github.com/ether/ep_add_hyperlink/workflows/Node.js%20Package/badge.svg)
+![Screenshot](https://user-images.githubusercontent.com/220864/107214131-5c3dd600-6a01-11eb-82d9-b2d67ec8ae93.png)
+## What is this?
+Etherpad plugin to add embedded hyperlinks in a pad.
+## Export support
+Experimental.  As a special attribute on Etherpad, some weirdness may be experienced.
+## History
+I decided to copy/paste the citizenos/ep_add_hyperlink to new plugin as it was not accessible anymore
+## License
+Apache 2
+## Author
+Ilmar Türk
+## Funding
+Funded by Citizen OS
---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const eejs = require('ep_etherpad-lite/node/eejs/');
+const settings = require('ep_etherpad-lite/node/utils/Settings');
+const {JSDOM} = require('jsdom');
+
+exports.padInitToolbar = (hookName, args, cb) => {
+  const toolbar = args.toolbar;
+  if (JSON.stringify(settings.toolbar).indexOf('addHyperlink') === -1) {
+    settings.toolbar.left.push(['addHyperlink']);
+  }
+  const button = toolbar.button({
+    command: 'addHyperlink',
+    localizationId: 'ep_add_hyperlink.editbarButtons.hyperlink.title',
+    class: 'buttonicon buttonicon-link hyperlink-icon',
+  });
+
+  toolbar.registerButton('addHyperlink', button);
+
+  return cb();
+};
+
+exports.eejsBlock_body = (hook, args, cb) => {
+  args.content += eejs.require('ep_add_hyperlink/templates/popup.ejs', {}, module);
+  return cb();
+};
+
+
+// Add the props to be supported in export
+exports.exportHtmlAdditionalTagsWithData = async (hook, pad) => {
+  const ret = [];
+  pad.pool.eachAttrib((k, v) => { if (k === 'url') ret.push([k, v]); });
+  return ret;
+};
+
+
+exports.getLineHTMLForExport = async (hook, context) => {
+  const elem = JSDOM.fragment(context.lineContent);
+  const parseNode = async (node) => {
+    const attrs = node.attributes;
+
+    if (attrs) {
+      for (let i = 0; i < attrs.length; i++) {
+        const attr = attrs[i];
+        if (attr.name === 'data-url') {
+          const nodeHTML = node.outerHTML.trim();
+
+          const replaceHTML = (`${nodeHTML.substring(0, nodeHTML.length - 5)}a>`)
+              .replace('<span data-url', '<a href');
+          context.lineContent = JSDOM
+              .fragment(`<div>${context.lineContent}</div>`).firstChild.innerHTML
+              .replace(nodeHTML, replaceHTML);
+        }
+      }
+    }
+
+    if (node.childNodes) {
+      node.childNodes.forEach(async (child) => {
+        await parseNode(child);
+      });
+    }
+  };
+
+  await parseNode(elem);
+};
